f9dd8def5aba89855318a488711753c6a28c1e23
root <root@void.localdomain>
2020-11-05 08:59:30 +0900

    v1

---
+++ exceptions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n1, n2, r;
+	for(int k = 0; k < 4; k++){
+		cin >> n1;
+		cin >> n2;
+		r = n1 / n2;
+		cout << r << "\n";
+	}
+	return 0;
+}
+
