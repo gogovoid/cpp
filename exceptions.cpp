9937c3dbe985a2c81ae7b2a3132d164f9f7011fe
root <root@void.localdomain>
2020-11-05 09:51:12 +0900

    v4

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
+int q(int a, int b){
+	if(b == 0) return -1;
+	return a / b;
+}
 
 int main(){
 	int n1, n2, r;
 	for(int k = 0; k < 4; k++){
 		cin >> n1;
-		cin >> n2;
-		if(n2 == 0){
+		cin >> n2;		
+		r = q(n1, n2);
+		if(r == -1){
 			cout << "Can not divide by zero!" << endl;
 		}else{
-			r = n1 / n2;
 			cout << r << "\n";
 		}
 	}
