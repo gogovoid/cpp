a67899eed963a910e7ad99e5b1b6306130d21560
root <root@void.localdomain>
2020-11-05 11:10:55 +0900

    v6

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-/*
 int q(int a, int b){
-	if(b == 0) return -1;
+	if(b == 0) throw 0;
 	return a / b;
 }
-*/
 
 int main(){
 	int n1, n2, r;
@@ -13,27 +11,11 @@ int main(){
 		cin >> n1;
 		cin >> n2;		
 		try{
-			if(n2 == 0){
-				throw 0;
-			}
-			r = n1 / n2;
-			cout << r << "\n";
-		} catch(int x) {
+			cout << q(n1, n2) << "\n";
+		} catch(int x){
 			cout << "Can not divide by zero!" << endl;
 		}
 	}
-/*
-	for(int k = 0; k < 4; k++){
-		cin >> n1;
-		cin >> n2;		
-		r = q(n1, n2);
-		if(r == -1){
-			cout << "Can not divide by zero!" << endl;
-		}else{
-			cout << r << "\n";
-		}
-	}
-	*/
 	return 0;
 }
 
