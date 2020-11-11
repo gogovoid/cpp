d3554f820fdd6369c71490d77955e4c314a54dcf
root <root@void.localdomain>
2020-11-11 23:26:39 +0900

    stack unwind v4

--- stack_uw.cpp
+++ stack_uw.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 using namespace std;
+class Pl {
+	int x;
+public:
+	Pl() { cout << "Constructor\n"; }
+	~Pl() { cout << "Destructor\n"; }
+};
 
-void f1(int n1, int n2){
+void f2(int n1, int n2){
 	string e = "Divide by Zero Error!";
 	if(n2 == 0) throw e;
-	cout << "f1() start\n";
+	cout << n1/n2 << endl;
+}
+
+void f1(const string& e){
+	Pl pl;
+	f2(9, 0);
 }
 
 int main(){
 	try {
-		f1(9, 0);
-	//} catch (const string& err){
-	} catch (int code_no){
-		cout << code_no << "\n";
+		string s = "PL";
+		f1(s);
+	} catch (const string& err){
+	//} catch (int code_no){
+		cout << err << "\n";
 	}
+	cout << "Bye~\n";
+
 	return 0;
 }
 
